289768d18e4a24a028cca1808346328c05aa2ae9
Arshia Sharma <arsharma@chapman.edu>
2018-11-26 12:11:22 -0800

    Adds cars.sh

---
+++ cars.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#cars.sh
+#Arshia Sharma 
+
+continue="yes"
+
+while [ continue="yes" ]
+do
+echo "1. Enter car"
+echo "2. Show list of cars"
+echo "Q. Quit"
+
+
+read CHOICE 
+
+case $CHOICE in
+	1) echo "Enter in car year: "
+	   read YEAR 
+	   echo "Enter in the make of the car: "
+	   read MAKE 
+	   echo "Enter the model of the car: "
+	   read MODEL
+	   echo "$YEAR:$MAKE:$MODEL">>My_old_cars
+	   ;;
+
+	2) sort My_old_cars
+	;;
+
+	Q) echo "Goodbye"
+	   continue="no"
+	   break 
+	   ;;
+esac
+
+done 
